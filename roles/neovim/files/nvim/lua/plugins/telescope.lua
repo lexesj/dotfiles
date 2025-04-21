d9003a0c3907bcf2daef75987d5f68fd54f845bc
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2025-04-21 22:47:56 +0100

    feat: add buffer search for telescope

--- roles/neovim/files/nvim/lua/plugins/telescope.lua
+++ roles/neovim/files/nvim/lua/plugins/telescope.lua
@@ -5,6 +5,7 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim' },
   keys = {
     { "<leader>pf", builtin.find_files, desc = "telescope find files" },
+    { "<leader>pb", builtin.buffers, desc = "telescope buffers" },
     { "<leader>ps", builtin.live_grep, desc = "telescope live grep" },
     { "<C-p>", builtin.git_files, desc = "telescope git files" }
   }
