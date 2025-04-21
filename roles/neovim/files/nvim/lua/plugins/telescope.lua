25331bde4037ed086aec3d565b88b518c7e9c09a
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2025-04-21 22:36:50 +0100

    feat: add nvim-telescope

---
+++ roles/neovim/files/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,12 @@
+local builtin = require('telescope.builtin')
+
+return {
+  'nvim-telescope/telescope.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  keys = {
+    { "<leader>pf", builtin.find_files, desc = "telescope find files" },
+    { "<leader>ps", builtin.live_grep, desc = "telescope live grep" },
+    { "<C-p>", builtin.git_files, desc = "telescope git files" }
+  }
+}
+
