8408a60ad48a9bfd4349a03e34fefc30b33814ab
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2025-04-26 16:47:58 +0100

    feat: add keybinds to move between windows

--- roles/neovim/files/nvim/lua/config/remap.lua
+++ roles/neovim/files/nvim/lua/config/remap.lua
@@ -2,3 +2,11 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "open netrw" })
 
+-- move between windows 
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "go to left window", remap = "true" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "go to upper window", remap = "true" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "go to lower window", remap = "true" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "go to right window", remap = "true" })
+
+vim.keymap.set("n", "<leader>l", vim.cmd.Lazy, { desc = "open lazy" })
+
