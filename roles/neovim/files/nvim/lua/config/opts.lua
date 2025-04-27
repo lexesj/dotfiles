802a687a22cb0a70506e2af824320901c0e20b93
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2025-04-27 21:55:33 +0100

    feat: increase update time

--- roles/neovim/files/nvim/lua/config/opts.lua
+++ roles/neovim/files/nvim/lua/config/opts.lua
@@ -5,8 +5,9 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
-
 vim.opt.smartindent = true
 
 vim.opt.clipboard = "unnamedplus"
+
+vim.updatetime = 50
 
