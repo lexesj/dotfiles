7dc221ef8c9ef44c09952605d1e45de05b77c5f4
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2025-04-27 23:32:31 +0100

    Add keybind for diagnostics float

--- roles/neovim/files/nvim/lua/plugins/lsp.lua
+++ roles/neovim/files/nvim/lua/plugins/lsp.lua
@@ -20,7 +20,8 @@ return {
       { "gh", vim.lsp.buf.hover, desc = "lsp hover" },
       { "gd", vim.lsp.buf.definition, desc = "lsp go to definition" },
       { "gi", vim.lsp.buf.implementation, desc = "lsp go to implementation" },
-      { "<leader>ca", vim.lsp.buf.code_action, desc = "lsp code action" }
+      { "<leader>ca", vim.lsp.buf.code_action, desc = "lsp code action" },
+      { "<leader>cd", vim.diagnostic.open_float, desc = "lsp diagnostics" }
     }
   }
 }
