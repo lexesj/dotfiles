cdc8dfdd95543a32c92340a39de8544f74a898e6
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2025-05-19 23:31:36 +0000

    feat: add lsp formatting and virtual text diagnostics

--- roles/neovim/files/nvim/lua/plugins/lsp.lua
+++ roles/neovim/files/nvim/lua/plugins/lsp.lua
@@ -1,11 +1,11 @@
 return {
   {
     "mason-org/mason.nvim",
-    opts = {}
+    opts = {},
   },
   {
     "mason-org/mason-lspconfig.nvim",
-    opts = {}
+    opts = {},
   },
   {
     "neovim/nvim-lspconfig",
@@ -14,9 +14,22 @@ return {
       { "gh",         vim.lsp.buf.hover,          desc = "lsp hover" },
       { "gd",         vim.lsp.buf.definition,     desc = "lsp go to definition" },
       { "gi",         vim.lsp.buf.implementation, desc = "lsp go to implementation" },
+      { "<leader>gf", vim.lsp.buf.format,         desc = "lsp format" },
       { "<leader>ca", vim.lsp.buf.code_action,    desc = "lsp code action" },
-      { "<leader>cd", vim.diagnostic.open_float,  desc = "lsp diagnostics" }
-    }
-  }
+      { "<leader>cd", vim.diagnostic.open_float,  desc = "lsp diagnostics" },
+    },
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    dependencies = { "plenary.nvim" },
+    opts = function()
+      local null_ls = require("null-ls")
+      return {
+        sources = {
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.prettierd,
+        },
+      }
+    end,
+  },
 }
-
