cc2a7b26b5a519abddff770c1a4091b0b2919654
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2025-05-20 00:55:30 +0100

    style: format code

--- roles/neovim/files/nvim/lua/plugins/telescope.lua
+++ roles/neovim/files/nvim/lua/plugins/telescope.lua
@@ -1,29 +1,29 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
+    lazy = false,
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = function()
       return {
         ["ui-select"] = {
-          require("telescope.themes").get_dropdown()
-        }
+          require("telescope.themes").get_dropdown(),
+        },
       }
     end,
     keys = function()
-      local builtin = require('telescope.builtin')
+      local builtin = require("telescope.builtin")
       return {
         { "<leader>pf", builtin.find_files, desc = "telescope find files" },
-        { "<leader>pb", builtin.buffers, desc = "telescope buffers" },
-        { "<leader>ps", builtin.live_grep, desc = "telescope live grep" },
-        { "<C-p>", builtin.git_files, desc = "telescope git files" }
+        { "<leader>pb", builtin.buffers,    desc = "telescope buffers" },
+        { "<leader>ps", builtin.live_grep,  desc = "telescope live grep" },
+        { "<C-p>",      builtin.git_files,  desc = "telescope git files" },
       }
-    end
+    end,
   },
   {
     "nvim-telescope/telescope-ui-select.nvim",
     config = function()
       require("telescope").load_extension("ui-select")
-    end
-  }
+    end,
+  },
 }
-
