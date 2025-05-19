e0bb4ae870bbc9968641fb9ef19b7457f16dc7f9
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2025-05-19 21:19:11 +0000

    feat: lazy load opts and keys

--- roles/neovim/files/nvim/lua/plugins/telescope.lua
+++ roles/neovim/files/nvim/lua/plugins/telescope.lua
@@ -1,20 +1,23 @@
-local builtin = require('telescope.builtin')
-
 return {
   {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {
-      ["ui-select"] = {
-        require("telescope.themes").get_dropdown()
+    opts = function()
+      return {
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown()
+        }
       }
-    },
-    keys = {
-      { "<leader>pf", builtin.find_files, desc = "telescope find files" },
-      { "<leader>pb", builtin.buffers, desc = "telescope buffers" },
-      { "<leader>ps", builtin.live_grep, desc = "telescope live grep" },
-      { "<C-p>", builtin.git_files, desc = "telescope git files" }
-    }
+    end,
+    keys = function()
+      local builtin = require('telescope.builtin')
+      return {
+        { "<leader>pf", builtin.find_files, desc = "telescope find files" },
+        { "<leader>pb", builtin.buffers, desc = "telescope buffers" },
+        { "<leader>ps", builtin.live_grep, desc = "telescope live grep" },
+        { "<C-p>", builtin.git_files, desc = "telescope git files" }
+      }
+    end
   },
   {
     "nvim-telescope/telescope-ui-select.nvim",
