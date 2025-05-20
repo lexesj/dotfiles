74a7f50ef5442d7beec1db7113f919054eb06405
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2025-05-20 13:04:20 +0000

    style: format lua files, add <leader>cd for diagnostics

--- roles/neovim/files/nvim/lua/plugins/oil.lua
+++ roles/neovim/files/nvim/lua/plugins/oil.lua
@@ -1,9 +1,9 @@
 return {
-	"stevearc/oil.nvim",
-	opts = {},
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	lazy = false,
-	keys = {
-		{ "<leader>pv", "<CMD>Oil<CR>", desc = "open parent directory" },
-	},
+  "stevearc/oil.nvim",
+  opts = {},
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  lazy = false,
+  keys = {
+    { "<leader>pv", "<cmd>Oil<dr>", desc = "open parent directory" },
+  },
 }
