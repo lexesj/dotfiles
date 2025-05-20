369b487fe4daf01867c200d3f5ec3b0cb3fedb0e
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2025-05-20 00:41:08 +0000

    Add oil.nvim

---
+++ roles/neovim/files/nvim/lua/plugins/oil.lua
@@ -0,0 +1,9 @@
+return {
+  "stevearc/oil.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  lazy = false,
+  opts = {},
+  keys = {
+    { "-", "<CMD>Oil<CR>", desc = "open parent directory" },
+  },
+}
