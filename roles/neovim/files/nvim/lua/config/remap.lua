9df665c58eef2c91bf600d790dab69b3456650f2
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2025-05-20 08:34:40 +0000

    feat: add vim-tmux-navigator

--- roles/neovim/files/nvim/lua/config/remap.lua
+++ roles/neovim/files/nvim/lua/config/remap.lua
@@ -1,9 +1,3 @@
 vim.g.mapleader = " "
 
--- move between windows
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "go to left window", remap = true })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "go to upper window", remap = true })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "go to lower window", remap = true })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "go to right window", remap = true })
-
 vim.keymap.set("n", "<leader>l", vim.cmd.Lazy, { desc = "open lazy" })
