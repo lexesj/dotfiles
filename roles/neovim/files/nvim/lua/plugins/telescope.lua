fa2513fbd9c4f471d08db630b43afaf3ff30be11
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2025-05-20 15:36:09 +0000

    feat: switch to telescope bindings

--- roles/neovim/files/nvim/lua/plugins/telescope.lua
+++ roles/neovim/files/nvim/lua/plugins/telescope.lua
@@ -13,11 +13,14 @@ return {
     keys = function()
       local builtin = require("telescope.builtin")
       return {
-        { "<leader>pf", builtin.find_files,  desc = "telescope find files" },
-        { "<leader>pb", builtin.buffers,     desc = "telescope buffers" },
-        { "<leader>ps", builtin.live_grep,   desc = "telescope live grep" },
-        { "<C-p>",      builtin.git_files,   desc = "telescope git files" },
-        { "<leader>cd", builtin.diagnostics, desc = "telescope diagnostics" },
+        { "grr",        builtin.lsp_references,      desc = "telescope lsp references" },
+        { "gri",        builtin.lsp_implementations, desc = "telescope lsp implementations" },
+        { "gd",         builtin.lsp_definitions,     desc = "telescope lsp definitions" },
+        { "<leader>pf", builtin.find_files,          desc = "telescope find files" },
+        { "<leader>pb", builtin.buffers,             desc = "telescope buffers" },
+        { "<leader>ps", builtin.live_grep,           desc = "telescope live grep" },
+        { "<C-p>",      builtin.git_files,           desc = "telescope git files" },
+        { "<leader>cd", builtin.diagnostics,         desc = "telescope diagnostics" },
       }
     end,
   },
