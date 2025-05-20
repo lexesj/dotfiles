fc8937b31fed78b954bf3baa0d061455860f72a2
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2025-05-20 13:02:27 +0000

    Remove default keymaps

--- roles/neovim/files/nvim/lua/plugins/lsp.lua
+++ roles/neovim/files/nvim/lua/plugins/lsp.lua
@@ -11,12 +11,7 @@ return {
     "neovim/nvim-lspconfig",
     lazy = false,
     keys = {
-      { "gh",         vim.lsp.buf.hover,          desc = "lsp hover" },
-      { "gd",         vim.lsp.buf.definition,     desc = "lsp go to definition" },
-      { "gi",         vim.lsp.buf.implementation, desc = "lsp go to implementation" },
-      { "<leader>gf", vim.lsp.buf.format,         desc = "lsp format" },
-      { "<leader>ca", vim.lsp.buf.code_action,    desc = "lsp code action" },
-      { "<leader>cd", vim.diagnostic.open_float,  desc = "lsp diagnostics" },
+      { "gd", vim.lsp.buf.definition, desc = "lsp go to definition" },
     },
   },
   {
