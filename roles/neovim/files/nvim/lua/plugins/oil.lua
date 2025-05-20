1486c224208eb36dc3e82262b4f27a25ab590397
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2025-05-20 00:50:34 +0000

    refactor: move auto format code, switch file tree keybind

--- roles/neovim/files/nvim/lua/plugins/oil.lua
+++ roles/neovim/files/nvim/lua/plugins/oil.lua
@@ -1,9 +1,9 @@
 return {
-  "stevearc/oil.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  lazy = false,
-  opts = {},
-  keys = {
-    { "-", "<CMD>Oil<CR>", desc = "open parent directory" },
-  },
+	"stevearc/oil.nvim",
+	opts = {},
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	lazy = false,
+	keys = {
+		{ "<leader>pv", "<CMD>Oil<CR>", desc = "open parent directory" },
+	},
 }
