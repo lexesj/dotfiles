f91a00175d065751e7f22e3218af1f13d5180f34
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2025-05-22 22:11:17 +0000

    Fix add to path function

--- roles/zsh/files/zsh/before/path_functions.zsh
+++ roles/zsh/files/zsh/before/path_functions.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 function add_to_path() {
-  if [[ "$PATH" != "*$1*" ]]; then
-    export PATH="$PATH:$1"
+  if [[ "$PATH" != *"$1"* ]]; then
+    export PATH="$1:$PATH"
   fi
 }
