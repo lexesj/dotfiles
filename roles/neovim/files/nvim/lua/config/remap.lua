cf3ac680cdd984e64af9c504db5807b1f2cf47ae
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2025-05-24 22:04:20 +0100

    feat: add better movement with text overflow

--- roles/neovim/files/nvim/lua/config/remap.lua
+++ roles/neovim/files/nvim/lua/config/remap.lua
@@ -7,3 +7,17 @@ vim.keymap.set("n", "<M-,>", "<C-w>5<", { desc = "increase split width" })
 vim.keymap.set("n", "<M-.>", "<C-w>5>", { desc = "decrease split width" })
 vim.keymap.set("n", "<M-t>", "<C-w>+", { desc = "increase split height" })
 vim.keymap.set("n", "<M-s>", "<C-w>-", { desc = "increase split height" })
+
+-- Better movement with text overflow.
+local function vertial_move(key)
+	return function()
+		local count = vim.v.count
+		if count == 0 then
+			return "g" .. key
+		end
+		return key
+	end
+end
+
+vim.keymap.set("n", "j", vertial_move("j"), { expr = true, desc = "move vertically down" })
+vim.keymap.set("n", "k", vertial_move("k"), { expr = true, desc = "move vertically up" })
