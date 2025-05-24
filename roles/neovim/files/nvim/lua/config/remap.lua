70299faac67bcaf0ac96337fbf32275aacaf1924
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2025-05-24 22:02:02 +0100

    Add keymaps to change window size

--- roles/neovim/files/nvim/lua/config/remap.lua
+++ roles/neovim/files/nvim/lua/config/remap.lua
@@ -1,3 +1,9 @@
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>l", vim.cmd.Lazy, { desc = "open lazy" })
+
+-- Change split size.
+vim.keymap.set("n", "<M-,>", "<C-w>5<", { desc = "increase split width" })
+vim.keymap.set("n", "<M-.>", "<C-w>5>", { desc = "decrease split width" })
+vim.keymap.set("n", "<M-t>", "<C-w>+", { desc = "increase split height" })
+vim.keymap.set("n", "<M-s>", "<C-w>-", { desc = "increase split height" })
