8ea7e549518ec331d1c2c0301a3895f92b1c5036
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2025-05-24 10:09:37 +0100

    feat: add keymap for searching neovim config files, add telescope prefix automatically

--- roles/neovim/files/nvim/lua/plugins/telescope.lua
+++ roles/neovim/files/nvim/lua/plugins/telescope.lua
@@ -12,21 +12,34 @@ return {
 		end,
 		keys = function()
 			local builtin = require("telescope.builtin")
-			return {
-				{ "grr", builtin.lsp_references, desc = "telescope lsp references" },
-				{ "gri", builtin.lsp_implementations, desc = "telescope lsp implementations" },
-				{ "gd", builtin.lsp_definitions, desc = "telescope lsp definitions" },
-				{ "<leader>sh", builtin.help_tags, desc = "telescope [s]earch [h]elp" },
-				{ "<leader>sk", builtin.keymaps, desc = "telescope [s]earch [k]eymaps" },
-				{ "<leader>sf", builtin.find_files, desc = "telescope [s]earch [f]iles" },
-				{ "<leader>ss", builtin.builtin, desc = "telescope [s]earch [s]elect telescope" },
-				{ "<leader>sw", builtin.grep_string, desc = "telescope [s]earch current [w]ord" },
-				{ "<leader>sg", builtin.live_grep, desc = "telescope [s]earch by [g]rep" },
-				{ "<leader>sd", builtin.diagnostics, desc = "telescope [s]earch [d]iagnostics" },
-				{ "<leader>sr", builtin.resume, desc = "telescope [s]earch [r]esume" },
-				{ "<leader>s.", builtin.oldfiles, desc = 'telescope [s]earch recent files ("." for repeat)' },
-				{ "<leader><leader>", builtin.buffers, desc = "telescope [ ] find existing buffers" },
+			local keymaps = {
+				{ "grr", builtin.lsp_references, desc = "lsp references" },
+				{ "gri", builtin.lsp_implementations, desc = "lsp implementations" },
+				{ "gd", builtin.lsp_definitions, desc = "lsp definitions" },
+				{ "<leader>sh", builtin.help_tags, desc = "[s]earch [h]elp" },
+				{ "<leader>sk", builtin.keymaps, desc = "[s]earch [k]eymaps" },
+				{ "<leader>sf", builtin.find_files, desc = "[s]earch [f]iles" },
+				{ "<leader>ss", builtin.builtin, desc = "[s]earch [s]elect telescope" },
+				{ "<leader>sw", builtin.grep_string, desc = "[s]earch current [w]ord" },
+				{ "<leader>sg", builtin.live_grep, desc = "[s]earch by [g]rep" },
+				{ "<leader>sd", builtin.diagnostics, desc = "[s]earch [d]iagnostics" },
+				{ "<leader>sr", builtin.resume, desc = "[s]earch [r]esume" },
+				{ "<leader>s.", builtin.oldfiles, desc = '[s]earch recent files ("." for repeat)' },
+				{ "<leader><leader>", builtin.buffers, desc = "[ ] find existing buffers" },
+				{
+					"<leader>sn",
+					function()
+						builtin.find_files({ cwd = vim.fn.stdpath("config") })
+					end,
+					desc = "[s]earch [n]eovim files",
+				},
 			}
+
+			for _, keymap in ipairs(keymaps) do
+				keymap.desc = "telescope " .. keymap.desc
+			end
+
+			return keymaps
 		end,
 	},
 	{
