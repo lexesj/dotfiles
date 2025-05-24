9284e77a625c710ca6e505a895807a848695e821
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2025-05-24 17:23:51 +0100

    feat: enable more options and add descriptions

--- roles/neovim/files/nvim/lua/config/opts.lua
+++ roles/neovim/files/nvim/lua/config/opts.lua
@@ -7,8 +7,20 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.smartindent = true
 
+-- Global clipboard.
 vim.opt.clipboard = "unnamedplus"
 
-vim.updatetime = 50
+-- Decrease update time.
+vim.opt.updatetime = 250
 
 vim.opt.spell = true
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = "split"
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Show white space characters.
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
