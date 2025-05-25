0615706267e0ebe6011d4e8fc410bf1b2b09f1ca
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2025-05-25 12:31:21 +0100

    feat: make treesitter lazy load

--- roles/neovim/files/nvim/lua/plugins/treesitter.lua
+++ roles/neovim/files/nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	main = "nvim-treesitter.configs",
+	event = { "BufReadPost", "BufWritePost", "BufNewFile", "VeryLazy" },
 	opts = {
 		auto_install = true,
 		sync_install = false,
