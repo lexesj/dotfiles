e0384853b5f412bda52f7499efb4640756234288
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2025-06-02 22:02:33 +0100

    Merge command to format toggle

--- roles/neovim/files/nvim/lua/config/user_commands.lua
+++ roles/neovim/files/nvim/lua/config/user_commands.lua
@@ -1,18 +1,21 @@
-vim.api.nvim_create_user_command("FormatDisable", function(args)
+---@module "snacks"
+
+vim.api.nvim_create_user_command("FormatToggle", function(args)
+	if vim.b.disable_autoformat or vim.g.disable_autoformat then
+		vim.b.disable_autoformat = false
+		vim.g.disable_autoformat = false
+		Snacks.notify.info("Auto formatting enabled!")
+		return
+	end
+
 	if args.bang then
-		-- FormatDisable! will disable formatting just for this buffer
 		vim.b.disable_autoformat = true
+		Snacks.notify.info("Auto formatting disabled for this buffer!")
 	else
 		vim.g.disable_autoformat = true
+		Snacks.notify.info("Auto formatting globally disabled!")
 	end
 end, {
-	desc = "Disable autoformat-on-save",
-	bang = true,
-})
-
-vim.api.nvim_create_user_command("FormatEnable", function()
-	vim.b.disable_autoformat = false
-	vim.g.disable_autoformat = false
-end, {
 	desc = "Re-enable autoformat-on-save",
+	bang = true,
 })
