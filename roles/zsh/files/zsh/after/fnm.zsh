5b225d4e3d2e494b5390630ce42765e4c31266bf
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2025-06-12 13:36:56 +0000

    style: format code

--- roles/zsh/files/zsh/after/fnm.zsh
+++ roles/zsh/files/zsh/after/fnm.zsh
@@ -3,7 +3,7 @@
 fnm_path="$HOME/.local/share/fnm"
 
 if [[ -d "$fnm_path" ]]; then
-  add_to_path "$fnm_path"
+	add_to_path "$fnm_path"
 fi
 
 eval "$(fnm env --use-on-cd --shell zsh)"
--- roles/zsh/files/zsh/after/mason.zsh
+++ roles/zsh/files/zsh/after/mason.zsh
@@ -3,5 +3,5 @@
 mason_bin_path="$HOME/.local/share/nvim/mason/bin"
 
 if [[ -d "$mason_bin_path" ]]; then
-  add_to_path "$mason_bin_path"
+	add_to_path "$mason_bin_path"
 fi
