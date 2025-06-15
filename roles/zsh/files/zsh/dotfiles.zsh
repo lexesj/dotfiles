a8665ec29b87226f6fa712cd13d22d49ecf7ee30
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2025-06-15 13:22:19 +0100

    Fix dotfiles path

--- roles/zsh/files/zsh/dotfiles.zsh
+++ roles/zsh/files/zsh/dotfiles.zsh
@@ -2,6 +2,6 @@
 
 dotfiles_bin_path="$HOME/.dotfiles/bin"
 
-if [[ -d "dotfiles_bin_path" ]]; then
-	add_to_path "dotfiles_bin_path"
+if [[ -d "$dotfiles_bin_path" ]]; then
+	add_to_path "$dotfiles_bin_path"
 fi
