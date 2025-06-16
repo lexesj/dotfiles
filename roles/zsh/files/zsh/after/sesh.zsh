fe1e83f373190cdb91e8964b3497fdd4f6a9e17b
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2025-06-16 23:17:15 +0100

    feat: add hide duplicates flag

--- roles/zsh/files/zsh/after/sesh.zsh
+++ roles/zsh/files/zsh/after/sesh.zsh
@@ -5,7 +5,7 @@ function sesh-sessions() {
 		exec </dev/tty
 		exec <&1
 		local session
-		session=$(sesh list | fzf --height 40% --reverse --border-label ' sesh ' --border --prompt '⚡  ')
+		session=$(sesh list --tmux --config --zoxide --hide-duplicates | fzf --height 40% --reverse --border-label ' sesh ' --border --prompt '⚡  ')
 		zle reset-prompt >/dev/null 2>&1 || true
 		[[ -z "$session" ]] && return
 		sesh connect $session
