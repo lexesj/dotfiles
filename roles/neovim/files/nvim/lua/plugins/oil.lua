cff0b5b8d85c1fdd215aadaece6933f1bfce3da3
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2025-07-03 22:52:27 +0100

    chore: remove oil-git.nvim

--- roles/neovim/files/nvim/lua/plugins/oil.lua
+++ roles/neovim/files/nvim/lua/plugins/oil.lua
@@ -1,20 +1,14 @@
 return {
-	{
-		"stevearc/oil.nvim",
-		opts = {
-			keymaps = {
-				["<C-h>"] = false,
-				["<C-l>"] = false,
-			},
-		},
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		lazy = false,
-		keys = {
-			{ "-", "<cmd>Oil<cr>", desc = "open parent directory" },
+	"stevearc/oil.nvim",
+	opts = {
+		keymaps = {
+			["<C-h>"] = false,
+			["<C-l>"] = false,
 		},
 	},
-	{
-		"benomahony/oil-git.nvim",
-		opts = {},
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	lazy = false,
+	keys = {
+		{ "-", "<cmd>Oil<cr>", desc = "open parent directory" },
 	},
 }
