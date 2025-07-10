f20f0b43a1e9796798d7f6061d758bb332dbb287
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2025-07-10 22:35:03 +0100

    chore: rename lsp folder to language-support

--- roles/neovim/files/nvim/lua/config/lazy.lua
+++ roles/neovim/files/nvim/lua/config/lazy.lua
@@ -24,7 +24,7 @@ require("lazy").setup({
 	spec = {
 		-- import your plugins.
 		{ import = "plugins" },
-		{ import = "plugins.lsp" },
+		{ import = "plugins.language-support" },
 	},
 	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
