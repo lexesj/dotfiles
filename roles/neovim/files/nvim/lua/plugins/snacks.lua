4ec0d048cc5319d416339d7da12dba134243acf0
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2025-07-12 12:05:06 +0100

    feat: do not format keymaps in snacks.lua

--- roles/neovim/files/nvim/lua/plugins/snacks.lua
+++ roles/neovim/files/nvim/lua/plugins/snacks.lua
@@ -28,243 +28,64 @@ return {
 		statuscolumn = { enabled = true },
 	},
 	keys = function()
+		-- stylua: ignore
 		local most_used_keymaps = {
-			{
-				"<leader><space>",
-				function()
-					Snacks.picker.smart()
-				end,
-				desc = "[ ] smart find files",
-			},
-			{
-				"<leader>/",
-				function()
-					Snacks.picker.grep()
-				end,
-				desc = "[/] search by grep",
-			},
-			{
-				"<leader>,",
-				function()
-					Snacks.picker.buffers()
-				end,
-				desc = "[,] search buffers",
-			},
-			{
-				"<leader>:",
-				function()
-					Snacks.picker.command_history()
-				end,
-				desc = "[:] search command history",
-			},
-			{
-				"<leader>n",
-				function()
-					Snacks.picker.notifications()
-				end,
-				desc = "search [n]otification history",
-			},
-			{
-				"<leader>e",
-				function()
-					Snacks.explorer()
-				end,
-				desc = "file [e]explorer",
-			},
+			{ "<leader><space>", function() Snacks.picker.smart() end, desc = "[ ] smart find files", },
+			{ "<leader>/", function() Snacks.picker.grep() end, desc = "[/] search by grep", },
+			{ "<leader>,", function() Snacks.picker.buffers() end, desc = "[,] search buffers", },
+			{ "<leader>:", function() Snacks.picker.command_history() end, desc = "[:] search command history", },
+			{ "<leader>n", function() Snacks.picker.notifications() end, desc = "search [n]otification history", },
+			{ "<leader>e", function() Snacks.explorer() end, desc = "file [e]explorer", },
 		}
 
+		-- stylua: ignore
 		local find_file_keymaps = {
-			{
-				"<leader>ff",
-				function()
-					Snacks.picker.files()
-				end,
-				desc = "[f]ind [f]iles",
-			},
-			{
-				"<leader>fb",
-				function()
-					Snacks.picker.files()
-				end,
-				desc = "[f]ind files in [b]uffers",
-			},
-			{
-				"<leader>fn",
-				function()
-					Snacks.picker.smart({ cwd = vim.fn.stdpath("config") })
-				end,
-				desc = "[f]ind in [n]eovim config",
-			},
-			{
-				"<leader>fp",
-				function()
-					Snacks.picker.projects()
-				end,
-				desc = "[f]ind [p]rojects",
-			},
+			{ "<leader>ff", function() Snacks.picker.files() end, desc = "[f]ind [f]iles", },
+			{ "<leader>fb", function() Snacks.picker.files() end, desc = "[f]ind files in [b]uffers", },
+			{ "<leader>fn", function() Snacks.picker.smart({ cwd = vim.fn.stdpath("config") }) end, desc = "[f]ind in [n]eovim config", },
+			{ "<leader>fp", function() Snacks.picker.projects() end, desc = "[f]ind [p]rojects", },
 		}
 
+		-- stylua: ignore
 		local search_keymaps = {
-			{
-				"<leader>sc",
-				function()
-					Snacks.picker.command_history()
-				end,
-				desc = "[s]earch [c]ommand history",
-			},
-			{
-				"<leader>sd",
-				function()
-					Snacks.picker.diagnostics()
-				end,
-				desc = "[s]earch [d]iagnostics",
-			},
-			{
-				"<leader>sh",
-				function()
-					Snacks.picker.help()
-				end,
-				desc = "[s]earch [h]elp",
-			},
-			{
-				"<leader>sk",
-				function()
-					Snacks.picker.keymaps()
-				end,
-				desc = "[s]earch [k]eymaps",
-			},
-			{
-				"<leader>sp",
-				function()
-					Snacks.picker.pickers()
-				end,
-				desc = "[s]earch [p]ickers",
-			},
-			{
-				"<leader>sq",
-				function()
-					Snacks.picker.qflist()
-				end,
-				desc = "[s]earch [q]uickfix list",
-			},
-			{
-				'<leader>s"',
-				function()
-					Snacks.picker.registers()
-				end,
-				desc = "[s]earch [r]egisters",
-			},
+			{ "<leader>sc", function() Snacks.picker.command_history() end, desc = "[s]earch [c]ommand history", },
+			{ "<leader>sd", function() Snacks.picker.diagnostics() end, desc = "[s]earch [d]iagnostics", },
+			{ "<leader>sh", function() Snacks.picker.help() end, desc = "[s]earch [h]elp", },
+			{ "<leader>sk", function() Snacks.picker.keymaps() end, desc = "[s]earch [k]eymaps", },
+			{ "<leader>sp", function() Snacks.picker.pickers() end, desc = "[s]earch [p]ickers", },
+			{ "<leader>sq", function() Snacks.picker.qflist() end, desc = "[s]earch [q]uickfix list", },
+			{ '<leader>s"', function() Snacks.picker.registers() end, desc = "[s]earch [r]egisters", },
 			-- Grep.
-			{
-				"<leader>sB",
-				function()
-					Snacks.picker.grep_buffers()
-				end,
-				desc = "grep [s]each open [B]uffers",
-			},
-			{
-				"<leader>sb",
-				function()
-					Snacks.picker.lines()
-				end,
-				desc = "[s]earch buffer lines",
-			},
-			{
-				"<leader>sg",
-				function()
-					Snacks.picker.grep()
-				end,
-				desc = "[s]earch by [g]rep",
-			},
+			{ "<leader>sB", function() Snacks.picker.grep_buffers() end, desc = "grep [s]each open [B]uffers", },
+			{ "<leader>sb", function() Snacks.picker.lines() end, desc = "[s]earch buffer lines", },
+			{ "<leader>sg", function() Snacks.picker.grep() end, desc = "[s]earch by [g]rep", },
 		}
 
+		-- stylua: ignore
 		local lsp_keymaps = {
-			{
-				"grd",
-				function()
-					Snacks.picker.lsp_definitions()
-				end,
-				desc = "lsp [d]efinitions",
-			},
-			{
-				"gri",
-				function()
-					Snacks.picker.lsp_implementations()
-				end,
-				desc = "lsp [i]mplementations",
-			},
-			{
-				"grr",
-				function()
-					Snacks.picker.lsp_references()
-				end,
-				desc = "lsp [r]eferences",
-			},
-			{
-				"<leader>ss",
-				function()
-					Snacks.picker.lsp_symbols()
-				end,
-				desc = "[s]earch lsp [s]ymbols",
-			},
-			{
-				"<leader>sS",
-				function()
-					Snacks.picker.lsp_workspace_symbols()
-				end,
-				desc = "[s]earch workspace [S]ymbols",
-			},
+			{ "grd", function() Snacks.picker.lsp_definitions() end, desc = "lsp [d]efinitions", },
+			{ "gri", function() Snacks.picker.lsp_implementations() end, desc = "lsp [i]mplementations", },
+			{ "grr", function() Snacks.picker.lsp_references() end, desc = "lsp [r]eferences", },
+			{ "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "[s]earch lsp [s]ymbols", },
+			{ "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "[s]earch workspace [S]ymbols", },
 		}
 
+		-- stylua: ignore
 		local git_keymaps = {
-			{
-				"<leader>gg",
-				function()
-					Snacks.lazygit()
-				end,
-				desc = "open lazy[g]it",
-			},
+			{ "<leader>gg", function() Snacks.lazygit() end, desc = "open lazy[g]it", },
 		}
 
+		-- stylua: ignore
 		local buffer_keymaps = {
-			{
-				"<leader>bd",
-				function()
-					Snacks.bufdelete()
-				end,
-				desc = "[b]uffer [d]elete",
-			},
-			{
-				"<leader>bD",
-				function()
-					Snacks.bufdelete.all()
-				end,
-				desc = "[b]uffer [D]elete all",
-			},
-			{
-				"<leader>.",
-				function()
-					Snacks.scratch()
-				end,
-				desc = "[.] toggle scratch buffer",
-			},
-			{
-				"<leader>z",
-				function()
-					Snacks.zen.zoom()
-				end,
-				desc = "toggle [z]oom",
-			},
+			{ "<leader>bd", function() Snacks.bufdelete() end, desc = "[b]uffer [d]elete", },
+			{ "<leader>bD", function() Snacks.bufdelete.all() end, desc = "[b]uffer [D]elete all", },
+			{ "<leader>.", function() Snacks.scratch() end, desc = "[.] toggle scratch buffer", },
+			{ "<leader>z", function() Snacks.zen.zoom() end, desc = "toggle [z]oom", },
 		}
 
+		-- stylua: ignore
 		local terminal_keymaps = {
-			{
-				"<c-_>",
-				function()
-					Snacks.terminal()
-				end,
-				desc = "[<c-_>] toggle terminal",
-			},
+			{ "<c-_>", function() Snacks.terminal() end, desc = "[<c-_>] toggle terminal", },
 		}
 
 		local keymaps = {}
