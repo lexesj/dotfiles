5713c40e1561ab8a168dd735bf5afbe28a243ba4
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2026-03-03 08:46:06 +0000

    feat: add better navigation keymaps for scrolling

--- roles/neovim/files/nvim/lua/config/keymaps.lua
+++ roles/neovim/files/nvim/lua/config/keymaps.lua
@@ -4,3 +4,6 @@ vim.keymap.set("n", "<M-.>", "<C-w>5>", { desc = "decrease split width" })
 vim.keymap.set("n", "<M-t>", "<C-w>+", { desc = "increase split height" })
 vim.keymap.set("n", "<M-s>", "<C-w>-", { desc = "increase split height" })
 
+-- Better navigation.
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up and center" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down and center" })
