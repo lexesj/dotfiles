c4421ae4daa37cc0545bd8d44ea539daacdc8239
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2026-03-04 09:39:23 +0000

    Add keymaps for copying file paths to clipboard

--- roles/neovim/files/nvim/lua/config/keymaps.lua
+++ roles/neovim/files/nvim/lua/config/keymaps.lua
@@ -7,3 +7,15 @@ vim.keymap.set("n", "<M-s>", "<C-w>-", { desc = "increase split height" })
 -- Better navigation.
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up and center" })
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down and center" })
+
+-- File path retrieval.
+vim.keymap.set("n", "<leader>fp", function()
+	local path = vim.fn.expand("%:p")
+	vim.fn.setreg("+", path)
+	vim.notify("Copied path to clipboard: " .. path)
+end, { desc = "Copy File Path" })
+vim.keymap.set("n", "<leader>fr", function()
+	local path = vim.fn.expand("%:.")
+	vim.fn.setreg("+", path)
+	vim.notify("Copied relative path: " .. path)
+end, { desc = "Copy Relative File Path" })
