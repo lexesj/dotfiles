d9a49db5bddea78b4d4cd0c26971b5063a860829
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2026-03-08 10:09:24 +0000

    Refactor lsp config to separate by languages

--- roles/neovim/files/nvim/lua/plugins/lsp.lua
+++ roles/neovim/files/nvim/lua/plugins/lsp.lua
@@ -20,14 +20,5 @@ return {
 				},
 			},
 		},
-		---@type table<string, fun(server:string, opts: vim.lsp.Config):boolean?>
-		setup = {
-			bashls = function(_, opts)
-				local default_filetypes = require("lspconfig.configs.bashls").default_config.filetypes
-				local all_filetypes = vim.list_extend(opts.filetypes or {}, default_filetypes)
-				vim.list_extend(all_filetypes, { "zsh" })
-				opts.filetypes = all_filetypes
-			end,
-		},
 	},
 }
