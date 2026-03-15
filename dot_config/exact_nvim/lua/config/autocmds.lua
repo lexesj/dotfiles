66234cd28c7613a3c90e57e9cfd7ff2fe7b8a928
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2026-03-15 10:26:03 +0000

    feat: run chezmoi apply in chezmoi directory

--- dot_config/exact_nvim/lua/config/autocmds.lua
+++ dot_config/exact_nvim/lua/config/autocmds.lua
@@ -9,3 +9,15 @@ vim.api.nvim_create_autocmd("User", {
 
 -- Disable spell autocmd
 vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Chezmoi apply in the chezmoi directory.
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+	pattern = { os.getenv("HOME") .. "/.local/share/chezmoi/*" },
+	callback = function(ev)
+		local bufnr = ev.buf
+		local edit_watch = function()
+			require("chezmoi.commands.__edit").watch(bufnr)
+		end
+		vim.schedule(edit_watch)
+	end,
+})
