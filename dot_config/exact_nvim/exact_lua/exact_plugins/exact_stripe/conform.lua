9ef33285ab361ec2c068234a403f777e07d010d0
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2026-03-16 11:18:40 +0000

    chore: remove redundant formatter settings

--- dot_config/exact_nvim/exact_lua/exact_plugins/exact_stripe/conform.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/exact_stripe/conform.lua
@@ -7,17 +7,9 @@ return {
 		formatters_by_ft = {
 			bzl = { "buildifier" },
 			go = { "stripe_goimports", "gofmt" },
-			graphql = { "prettierd" },
-			html = { "prettierd" },
-			javascript = { "prettierd" },
-			javascriptreact = { "prettierd" },
-			json = { "prettierd" },
-			jsonc = { "prettierd" },
 			scala = { "zoolander_format_scala" },
 			sql = { "zoolander_format_sql" },
 			terraform = { "sc_terraform" },
-			typescript = { "prettierd" },
-			typescriptreact = { "prettierd" },
 		},
 	},
 }
