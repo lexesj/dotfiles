73de6d81670e6cfc46505f7dd0c18cbdf84644fa
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2026-03-21 18:07:48 +0000

    fix(sesh): add command check

--- dot_config/exact_zsh/exact_after/sesh.zsh
+++ dot_config/exact_zsh/exact_after/sesh.zsh
@@ -1,17 +1,19 @@
 #!/usr/bin/env zsh
 
-function sesh-sessions() {
-	{
-		exec </dev/tty
-		exec <&1
-		local session
-		session=$(sesh list --tmux --config --zoxide --hide-duplicates | fzf --height 40% --reverse --border-label ' sesh ' --border --prompt '⚡  ')
-		zle reset-prompt >/dev/null 2>&1 || true
-		[[ -z "$session" ]] && return
-		sesh connect $session
+if command -v sesh &>/dev/null; then
+	function sesh-sessions() {
+		{
+			exec </dev/tty
+			exec <&1
+			local session
+			session=$(sesh list --tmux --config --zoxide --hide-duplicates | fzf --height 40% --reverse --border-label ' sesh ' --border --prompt '⚡  ')
+			zle reset-prompt >/dev/null 2>&1 || true
+			[[ -z "$session" ]] && return
+			sesh connect $session
+		}
 	}
-}
 
-zle -N sesh-sessions
-bindkey -M vicmd '\es' sesh-sessions
-bindkey -M viins '\es' sesh-sessions
+	zle -N sesh-sessions
+	bindkey -M vicmd '\es' sesh-sessions
+	bindkey -M viins '\es' sesh-sessions
+fi
