ae199e573191bd872526acba7284302d8c1e8987
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2026-03-22 14:21:56 +0000

    fix(nvim): disable autogen watching

--- dot_config/exact_nvim/exact_lua/exact_plugins/exact_stripe/pay.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/exact_stripe/pay.lua
@@ -1,5 +1,10 @@
 return {
 	url = "https://git.corp.stripe.com/iheanyi/pay.nvim",
 	dependencies = { "stevearc/overseer.nvim" },
-	opts = {},
+	opts = {
+		-- Autogen keeps rebooting. Disabling it for now.
+		autogen = {
+			auto_watch = false,
+		},
+	},
 }
