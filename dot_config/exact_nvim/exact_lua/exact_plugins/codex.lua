c8d8cd2268a898ea1bb2ffc1eccb6e08de476099
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2026-04-18 13:33:13 +0100

    feat(nvim): add better integration with codex

--- dot_config/exact_nvim/exact_lua/exact_plugins/codex.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/codex.lua
@@ -1,14 +1,38 @@
 return {
-	"kkrampis/codex.nvim",
-	cmd = { "Codex", "CodexToggle" },
+	"pittcat/codex.nvim",
+	cmd = { "CodexToggle", "CodexSendPath", "CodexSendSelection" },
 	keys = {
 		{
 			"<leader>ao",
 			-- stylua: ignore
 			function() require("codex").toggle() end,
-			desc = "Toggle Codex popup or side-panel",
+			desc = "Toggle Codex side-panel",
 			mode = { "n", "t" },
 		},
+		{
+			"<leader>aos",
+			":'<,'>CodexSendSelection<cr>",
+			desc = "Send selected text to Codex",
+			mode = { "v" },
+		},
+		{
+			"<leader>ap",
+			":CodexSendPath<cr>",
+			desc = "Send selected path to Codex",
+			mode = { "n" },
+		},
 	},
-	opts = {},
+	opts = {
+		terminal = {
+			provider = "auto",
+			direction = "vertical",
+			size = 0.40,
+		},
+		terminal_bridge = {
+			path_format = "abs",
+			path_prefix = "@",
+			auto_attach = true,
+			selection_mode = "reference",
+		},
+	},
 }
