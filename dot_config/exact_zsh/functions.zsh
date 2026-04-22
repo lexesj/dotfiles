3891e8ab03e448743f96f9218f3833115fa9b732
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2026-04-22 08:25:39 +0100

    feat(zsh): add devbox wrapper function

---
+++ dot_config/exact_zsh/functions.zsh
@@ -0,0 +1,37 @@
+new_devbox() {
+	local fe=0
+	local name=""
+
+	for arg in "$@"; do
+		case "$arg" in
+		--fe) fe=1 ;;
+		--help | -h)
+			echo "Usage: new_devbox <name> [--fe]"
+			echo ""
+			echo "Arguments:"
+			echo "  <name>   Name of the devbox (required)"
+			echo "  --fe     Include the dashboard-fe-srv/base graph"
+			return 0
+			;;
+		-*) echo "Unknown option: $arg" && return 1 ;;
+		*) name="$arg" ;;
+		esac
+	done
+
+	if [[ -z "$name" ]]; then
+		echo "Error: devbox name is required"
+		echo ""
+		echo "Usage: new_devbox <name> [--fe]"
+		return 1
+	fi
+
+	local cmd=(pay remote new --ide none --notify-on-ready --ssh --tmux --repo=mint --workspace=pay-server)
+
+	if [[ $fe -eq 1 ]]; then
+		cmd+=(--graph dashboard-fe-srv/base)
+	fi
+
+	cmd+=(-y "$name")
+
+	"${cmd[@]}"
+}
