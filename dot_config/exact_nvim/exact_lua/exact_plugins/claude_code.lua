da843a0324623d06669bbc02013647bf77d06e30
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2026-04-26 16:24:49 +0100

    feat(nvim): add claude code plugin with terminal split config

---
+++ dot_config/exact_nvim/exact_lua/exact_plugins/claude_code.lua
@@ -0,0 +1,12 @@
+---@module 'claudecode.types'
+
+return {
+	"coder/claudecode.nvim",
+	---@type PartialClaudeCodeConfig
+	opts = {
+		---@diagnostic disable-next-line: missing-fields
+		terminal = {
+			split_width_percentage = 0.4,
+		},
+	},
+}
